8d6abee982c7c6709da4b99f0463290f770e2b75
soso <sof.benzaied@gmail.com>
2019-02-14 15:40:58 +0100

    Etape7 fonctionnel

--- Checklists/Controllers/ChecklistItem.swift
+++ Checklists/Controllers/ChecklistItem.swift
@@ -13,9 +13,11 @@ class ChecklistItem
     var message :String
     var verif   :Bool
     
-    init(message: String, verif flag: Bool = false){
+    init(message: String, verif: Bool=false){
         self.message=message
         self.verif=verif
+        
+        
     }
   
 }
