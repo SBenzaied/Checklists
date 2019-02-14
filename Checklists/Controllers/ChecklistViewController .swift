c2bcc6127e5fec0665718c3fe4cfe85fa2e21af4
soso <sof.benzaied@gmail.com>
2019-02-14 16:51:24 +0100

    Etape9

--- Checklists/Controllers/ChecklistViewController .swift
+++ Checklists/Controllers/ChecklistViewController .swift
@@ -10,29 +10,50 @@ import UIKit
 
 
 
-class ChecklistViewController : UITableViewController {
 
+class ChecklistViewController : UITableViewController {
+    
+  var tabCheckListItem=[ChecklistItem]()
+
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        ChecklistItem(message: "test")
-        ChecklistItem(message: "Test",verif: true)
+        tabCheckListItem.append(ChecklistItem(message: "test"))
+        tabCheckListItem.append(ChecklistItem(message: "Test",verif: true))
+        tabCheckListItem.append(ChecklistItem(message: "MATCH",verif: true))
     }
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int{
-        return 1
+        return tabCheckListItem.count
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell{
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ChecklistItem", for: indexPath)
+        //cell.textLabel?.text = "message"
         
-    let cell = tableView.dequeueReusableCell(withIdentifier: "ChecklistItem", for: indexPath)
-        cell.textLabel?.text = "message"
+        
+        configureText(for: cell, withItem: tabCheckListItem[indexPath.row])
+        configureCheckmark(for: cell, withItem: tabCheckListItem[indexPath.row])
+    
         
         return cell
     }
     
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         tableView.deselectRow(at: indexPath, animated: true)
+    }
+    
+    func configureCheckmark(for cell: UITableViewCell, withItem item: ChecklistItem){
+        if item.verif == false{
+            cell.accessoryType = .none
+        }
+        else{cell.accessoryType = .checkmark}
+        
+        
+    }
+    func configureText(for cell: UITableViewCell, withItem item: ChecklistItem){
+        cell.textLabel?.text=item.message
     }
     
     
